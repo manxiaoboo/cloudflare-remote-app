8dc926a6f8014b5463c76b0051af03bf899c16d8
manxiaoboo <manxiaobooo@126.com>
2025-11-07 17:22:31 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,9 @@
-const {
-  shareAll,
-  ModuleFederationPlugin,
-} = require('@angular-architects/module-federation/webpack');
+const { shareAll, withModuleFederationPlugin } =
+  require('@angular-architects/module-federation/webpack');
 
-// 关键：开启 ESM 输出
 module.exports = {
-  experiments: { outputModule: true },
-  output: {
-    module: true,
-    scriptType: 'module',
-    library: { type: 'module' },
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'cloudflare-remote-app',
+  ...withModuleFederationPlugin({
+   name: 'cloudflare-remote-app',
 
       exposes: {
         './Books': './src/app/remote-entry/remote-books.component.ts',
@@ -22,6 +12,10 @@ module.exports = {
       shared: {
         ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
       },
-    }),
-  ],
+  }),
+  experiments: { outputModule: true },
+  output: {
+    scriptType: 'module',
+    library: { type: 'module' },
+  },
 };
